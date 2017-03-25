4cac6b24ba5011db087cd53ce1f327c0526ad387
Tiago Griffo <tgriffo@Tiagos-MacBook-Pro.local>
2017-03-25 13:29:49 +1000

    emotion api working

--- iPadApp/index.ios.js
+++ iPadApp/index.ios.js
@@ -13,6 +13,8 @@ import {
 import { Container, Content, ActionSheet, Button, Text, Header, Left, Right, Body, Title } from 'native-base';
 import Camera from 'react-native-camera';
 
+import { RNS3 } from 'react-native-aws3';
+
 export default class iPadApp extends Component {
   constructor(props) {
     super(props);
@@ -61,12 +63,59 @@ export default class iPadApp extends Component {
           debugCamera: path,
           lastImage: { uri: path } 
         });
+        this.uploadPicture(path);
       })
       .catch(err => this.setState({ debugCamera: 'capture error: ' + JSON.stringify(err) }));
   }
 
-  uploadPicture() {
-         
+  uploadPicture(path) {
+    //const parsedUrl = URL.parse(path);
+    //const name = parsedUrl.query.substring(3,parsedUrl.query.length-8);
+    //const type = 'image/' + parsedUrl.query.substring(parsedUrl.query.length-3);
+    const file = {
+      uri: path,
+      name: 'HELLO.jpg',
+      type: 'image/JPG'
+    };
+    const options = {
+      //keyPrefix: './',
+      bucket: 'flightcentre-hackathon-os-piratas',
+      region: 'ap-southeast-2',
+      accessKey: 'AKIAJQ3M4OYQX6UJ227A',
+      secretKey: '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',
+      successActionStatus: 201
+    };
+    RNS3.put(file, options)
+      .then(response => {
+        const imageCloudPath = response.body.postResponse.location;
+        this.setState({ 
+          debugCamera: imageCloudPath,
+          lastImage: { uri: imageCloudPath }
+        });
+        this.verifyEmotion(imageCloudPath);
+      })
+      .catch(err => this.setState({ debugCamera: 'upload error: ' + JSON.stringify(err) }));
+  }
+  
+  verifyEmotion(imageCloudPath) {
+    const body = { url: imageCloudPath };
+    const myHeaders = new Headers({
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': 'f07f680a18e94bd4a68a2f72884522bf'
+    });
+    const options = {
+      method: 'POST',
+      headers: myHeaders,
+      body: JSON.stringify(body)
+    };
+    const myRequest = new Request('https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize', options);
+    fetch(myRequest)
+    .then(response => {
+      return response.text();
+    })
+    .then(blob => {
+      this.setState({ debugCamera: blob });
+    });
   }
 
 }
